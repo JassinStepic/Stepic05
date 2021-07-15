1d556855f25443b0d6a46c90f303c0d229a897e3
JassinStepic <87114435+JassinStepic@users.noreply.github.com>
2021-07-15 16:50:51 +0300

    Update init.sh

--- web/init.sh
+++ web/init.sh
@@ -1,2 +1,3 @@
-sudo ln -sf /home/jassin/box/jassin/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+udo ln -sf /home/box/jassin/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
